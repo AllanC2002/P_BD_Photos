2edf67120f5b9149e86283064e393529c1fc6d6d
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-06-20 21:18:40 -0500

    first commit: db Photos mongo
    we added inti.js, dockerfile and workflows

---
+++ init.js
@@ -0,0 +1,9 @@
+db = db.getSiblingDB("Photos");
+
+db.createCollection("Images");
+
+db.Images.insertOne({
+  name: "Default photo",
+  url: "https://icones.pro/wp-content/uploads/2021/02/icone-utilisateur.png",
+  description: "Initial Image For Users"
+});
